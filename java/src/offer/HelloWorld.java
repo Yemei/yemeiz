c6c6aefeca29cffbd46f2bb077299db9fac0c959
Administrator <1312477469@qq.com>
2017-09-16 16:05:46 +0800

    Hello World!

---
+++ java/src/offer/HelloWorld.java
@@ -0,0 +1,10 @@
+package offer;
+
+/**
+ * Created by Administrator on 2017/9/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
